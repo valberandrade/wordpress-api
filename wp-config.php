bf4fcd615cc00a60b8fdef04aa14eea5b55c7b66
valberandrade <valber.andrade@hotmail.com>
2026-04-07 16:33:50 -0300

    Alterei o wp-config

--- wp-config.php
+++ wp-config.php
@@ -19,7 +19,7 @@
  */
 
 // ** Database settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
+/** The name of the DB for WordPress */
 define( 'DB_NAME', 'wordpress_api' );
 
 /** Database username */
